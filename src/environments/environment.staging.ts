4966dbfaeac4b0a2363c93747ec2979e9e0ce2f2
ZenMSI <zaenal.virus@gmail.com>
2025-11-03 16:23:15 +0700

    fixed incorect api url

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://custom-dashboard.karyaoptima.com',
-    assetUrl: 'https://custom-dashboard.karyaoptima.com/uploads/', //Always add slash at end of the line
+    apiUrl: 'https://custom-dashboard.karyaoptima.com/api',
+    assetUrl: 'https://custom-dashboard.karyaoptima.com/api/uploads/', //Always add slash at end of the line
     appName: '_csd.stg',
     appKey: 'Z9AMMw0MM0sIFbK3QX3QvDi7SdMPWgbE',
 };
